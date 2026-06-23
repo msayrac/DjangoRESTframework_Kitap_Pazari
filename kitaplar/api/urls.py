9bf5474550e92b778d7074e2baa79087385a03f7
msayracode <msayracode@gmail.com>
2026-06-23 13:25:01 +0300

    ConcreteViews

--- kitaplar/api/urls.py
+++ kitaplar/api/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from kitaplar.api import views as api_views
 urlpatterns = [
     path('kitaplar/', api_views.KitapListCreateAPIView.as_view(), name='kitap-listesi'),
+    path('kitaplar/<int:pk>', api_views.KitapDetailAPIView.as_view(), name='kitap-bilgileri'),
 ]
+
--- kitaplar/api/views.py
+++ kitaplar/api/views.py
@@ -1,20 +1,35 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
+from rest_framework import generics
+
 from kitaplar.api.serializers import KitapSerializer, YorumSerializer
 from kitaplar.models import Kitap
 
-class KitapListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+# Concrete view
+class KitapListCreateAPIView(generics.ListCreateAPIView):
     queryset = Kitap.objects.all()
     serializer_class = KitapSerializer
 
-    # Listelemek
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)        
 
-    #Yaratabilmek
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+class KitapDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Kitap.objects.all()
+    serializer_class = KitapSerializer
+
+
+
+# # GenericAPIView
+# class KitapListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+#     queryset = Kitap.objects.all()
+#     serializer_class = KitapSerializer
+
+#     # Listelemek
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)        
+
+#     #Yaratabilmek
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 
 
